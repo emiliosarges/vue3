c9e269bda4b5b4b195a6ce5228dc03695d971830
Emílio Silva <emiliosarges@gmail.com>
2025-02-27 23:48:27 -0300

    Primeiro Componente
    Criando o primeiro componente

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <PrimeiroComponente />
+</template>
+
+<script setup>
+  import PrimeiroComponente from './components/PrimeiroComponente.vue';
+/*  Obs: Não é mais necessário usar o export default, basta colocar o "setup" dentro da tag script: <script setup> :
+
+ export default {                
+    name: 'App',
+    components: {
+      PrimeiroComponente
+    }
+  }
+*/
+</script>
---
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Olá Vuee!</h1>
+</template>
+
+<script setup>
+/*  Obs: Não é mais necessário usar o export default, basta colocar o "setup" dentro da tag script: <script setup> . Nesse caso aqui, teoricamente funcionaria até sem o <script setup>
+
+  export default {
+        name: 'PrimeiroComponente'
+    } 
+*/
+</script>
